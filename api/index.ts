9c9b2108036e109360961c06c28b8495dd51636e
eimaam <imamddahir@gmail.com>
2024-07-17 22:06:23 +0100

    chore: Update server startup to use connectMongo utility

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,32 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import router from "../routes";
+import { connectMongo } from "../utils/connectMongo";
+
+dotenv.config()
 
 const PORT = process.env.PORT || 9000;
 
-const app = express();
+const app = express(); 
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", router);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectMongo();
+    
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log("Failed to Start Server", error);
+    process.exit(1);
+  }
+}
+
+startServer();

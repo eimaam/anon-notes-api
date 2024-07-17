610dbeb3042f18e20de12657980b5adeee1aa61d
eimaam <imamddahir@gmail.com>
2024-07-17 23:04:26 +0100

    FEAT: setup route/endpoints for all user related actions

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,18 @@
-import { Router } from 'express';
-import { Request, Response } from 'express';
+import { Router } from "express";
+import { Request, Response } from "express";
+import { UserController } from "../handlers/user";
 
 const router = Router();
 
-router.get('/healthcheck', (_req:Request, res:Response) => {
-  res.status(200).json({ success: true, message: 'API is running' });
-})
+router.get("/healthcheck", (_req: Request, res: Response) => {
+  res.status(200).json({ success: true, message: "API is running" });
+});
 
-export default router
+// user routes
+router.post("/user", UserController.create);
+router.get("/user/:userId", UserController.getById);
+router.get("/users", UserController.getAll);
+router.patch("/user/:userId", UserController.update);
+router.delete("/user/:userId", UserController.delete);
+
+export default router;

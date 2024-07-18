b32df1eae918410d02493954461a7379d22320eb
eimaam <imamddahir@gmail.com>
2024-07-18 22:09:42 +0100

    install express validation middleware, setup rate limiting for api protection

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,13 @@
+/**
+ * Application's entry point
+ */
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "../routes";
 import { connectMongo } from "../utils/connectMongo";
+import rateLimit from "express-rate-limit";
 
 dotenv.config()
 
@@ -12,6 +17,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Apply rate limiting middleware to limit the number of requests from an IP address
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 40,
+  message: "Too many requests from this IP, please try again in an hour",
+}))
 
 app.use("/api/v1", router);
 
